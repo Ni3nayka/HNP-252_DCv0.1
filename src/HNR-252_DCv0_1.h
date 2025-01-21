696f0195e053239937bc371cd3113088cd648fc3
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2025-01-21 09:26:48 +0300

    start

---
+++ src/HNR-252_DCv0_1.h
@@ -0,0 +1,16 @@
+/*
+   https://github.com/Ni3nayka/HNR-252_DCv0.1
+
+   author: Egor Bakay <egor_bakay@inbox.ru> Ni3nayka
+   write:  Jan 2025
+   modify: Jan 2025
+*/  
+
+#pragma once
+
+#ifndef (defined(ESP32)) 
+#error "lib not supported this board"
+#endif
+
+
+
